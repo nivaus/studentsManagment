937f9d625ef427d51e532e03cfdd08df9bd4302e
mtafinal <nivau@mail.mta.ac.il>
2016-01-20 11:48:37 +0200

    Fixed fields must not be empty issue. Cleaned console.log messages

--- web/js/script.js
+++ web/js/script.js
@@ -1,45 +1,41 @@
 /**
  * Created by oslander on 16/01/2016.
  */
-// create the module and name it scotchApp
 var studentsManagementApp = angular.module('studentsManagementApp', ['smart-table', 'ngRoute']);
 
 // configure our routes
-studentsManagementApp.config(function($routeProvider) {
+studentsManagementApp.config(function ($routeProvider) {
     $routeProvider
 
         // route for the home page
         .when('/', {
-            templateUrl : './pages/login.html',
-            controller  : 'loginController'
+            templateUrl: './pages/login.html',
+            controller: 'loginController'
         })
 
         // route for the about page
         .when('/studentsList', {
-            templateUrl : './pages/studentsList.html',
-            controller  : 'studentsList'
+            templateUrl: './pages/studentsList.html',
+            controller: 'studentsList'
         })
 
 });
 
-// create the controller and inject Angular's $scope
-studentsManagementApp.controller('loginController', function($scope) {
+// Login Controller
+studentsManagementApp.controller('loginController', function ($scope) {
     // create a message to display in our view
-    $scope.submit = function() {
-        console.log($scope.username);
-        console.log($scope.password);
+    $scope.submit = function () {
         $.ajax({
             data: "username=" + $scope.username + "&password=" + $scope.password,
             url: "login",
             method: "post",
             timeout: 2000,
-            error: function() {
+            error: function () {
                 console.log("Failed to send ajax");
             },
-            success: function(r) {
+            success: function (r) {
                 var response = JSON.parse(r);
-                if (response.result == "FAIL")
-                {
+                if (response.result == "FAIL") {
                     $("#badPasswordAlert").show();
                 }
                 else {
@@ -51,12 +47,13 @@ studentsManagementApp.controller('loginController', function($scope) {
     };
 });
 
-studentsManagementApp.controller('studentsList' ,function($scope) {
+//Students List Controller
+studentsManagementApp.controller('studentsList', function ($scope) {
     $scope.loggedInUser = JSON.parse(localStorage.getItem("loggedInUser")).data;
     $scope.students = [];
     $scope.selectedStudent = {};
-    $scope.updateSelectedStudent = function (student)
-    {
+    $scope.errorMessage = "";
+    $scope.updateSelectedStudent = function (student) {
         $scope.selectedStudent = JSON.parse(JSON.stringify(student));
     };
 
@@ -65,60 +62,47 @@ studentsManagementApp.controller('studentsList' ,function($scope) {
             data: "studentsList=true",
             url: "studentsDetails",
             timeout: 2000,
-            error: function() {
+            error: function () {
                 console.log("Failed to send ajax");
             },
-            success: function(r) {
-                console.log(r);
-                $scope.students= JSON.parse(r).data;
-                console.log($scope.students);
-
-                $scope.$apply();
-            }
-        });
-    };
-
-    $scope.editStudent = function (student, index)
-    {
-        console.log(index);
-        console.log($scope.students[index]);
-
-    };
-
-    $scope.removeStudentFromDB = function ()
-    {
-        console.log("remove");
-        $.ajax({
-            data: "removeStudent=" +  JSON.stringify($scope.selectedStudent),
-            url: "studentsDetails",
-            type: "POST",
-            error: function() {
-                console.log("Failed to send ajax");
-            },
-            success: function(r) {
-                console.log("success");
-                console.log(r);
+            success: function (r) {
                 $scope.students = JSON.parse(r).data;
                 $scope.$apply();
             }
         });
     };
-    $scope.updateUserDetailsInDB = function ()
-    {
-        console.log("update");
+
+
+    $scope.removeStudentFromDB = function () {
         $.ajax({
-            data: "updateStudent=" +  JSON.stringify($scope.selectedStudent),
+            data: "removeStudent=" + JSON.stringify($scope.selectedStudent),
             url: "studentsDetails",
             type: "POST",
-            error: function() {
+            error: function () {
                 console.log("Failed to send ajax");
             },
-            success: function(r) {
-                console.log("success");
-                console.log(r);
+            success: function (r) {
                 $scope.students = JSON.parse(r).data;
                 $scope.$apply();
             }
         });
     };
+
+    $scope.updateUserDetailsInDB = function () {
+        $("#newStudentModal").modal("hide");
+        $("#updateStudentModal").modal("hide");
+
+        $.ajax({
+            data: "updateStudent=" + JSON.stringify($scope.selectedStudent),
+            url: "studentsDetails",
+            type: "POST",
+            error: function () {
+                console.log("Failed to send ajax");
+            },
+            success: function (r) {
+                $scope.students = JSON.parse(r).data;
+                $scope.$apply();
+            }
+        });
+    }
 });
